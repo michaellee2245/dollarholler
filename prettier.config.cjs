85a4a31eb29106d85394acf838b6d0ed7269c525
Michael Lee <michaellee@Michaels-MacBook-Pro.local>
2023-02-27 16:52:23 -0700

    invoices line items displaying dynamic content and adding total amounts of multiple invoices line items per client

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+	useTabs: true,
+	singleQuote: true,
+	trailingComma: "none",
+	printWidth: 100,
+	plugins: [require('prettier-plugin-tailwindcss')],
+	pluginSearchDirs: ["."],
+	overrides: [{ "files": "*.svelte", "options": { "parser": "svelte" } }],
+	tailwindConfig: './tailwind.config.cjs'
+}
