96a91a79baaae9e544dd1cf7cea4568e23750fc4
Michael Lee <michaellee2245@gmail.com>
2023-05-10 19:13:41 -0600

    added mobile styles for individual invoice pages

--- src/routes/(dashboard)/invoices/[id]/+page.svelte
+++ src/routes/(dashboard)/invoices/[id]/+page.svelte
@@ -33,9 +33,11 @@
 	};
 </script>
 
-<div class="fixed z-0 mb-16 flex w-full max-w-screen-lg justify-between">
+<div
+	class="fixed z-0 mb-16 flex w-full max-w-screen-lg flex-col justify-between gap-y-5 px-4 md:flex-row lg:px-0"
+>
 	<h1 class="text-3xl font-bold text-daisyBush">Invoice</h1>
-	<div class="flex items-center gap-4">
+	<div class="flex flex-wrap items-center gap-2 sm:flex-nowrap sm:gap-4">
 		<Button
 			height="short"
 			label="Print"
@@ -47,7 +49,7 @@
 			height="short"
 			label={copyLinkLabel}
 			onClick={copyLink}
-			className="min-w-[168px] justify-center"
+			className="min-w-[100px] sm:min-w-[168px] justify-center"
 		/>
 		<Button height="short" label="Send" onClick={sendInvoice} />
 		<Button height="short" label="Pay Invoice" onClick={payInvoice} />
@@ -55,9 +57,9 @@
 </div>
 
 <div
-	class="relative top-32 z-10 grid grid-cols-6 gap-x-5 gap-y-8 bg-white py-16 px-32 shadow-invoice"
+	class="relative top-32 z-10 grid grid-cols-6 gap-x-5 gap-y-8 bg-white py-8 px-5 shadow-invoice md:py-16 md:px-32"
 >
-	<div class="col-span-3">
+	<div class="col-span-6 sm:col-span-3">
 		<img
 			src="/images/logo.png"
 			srcset="/images/logo@2x.png 2x, /images/logo.png 1x"
@@ -65,7 +67,7 @@
 		/>
 	</div>
 
-	<div class="col-span-2 col-start-5 pt-4">
+	<div class="col-span-6 pt-4 sm:col-span-2 sm:col-start-5">
 		{#if $settings && $settings.myName}
 			<div class="label">From</div>
 			<p>
@@ -85,7 +87,7 @@
 		{/if}
 	</div>
 
-	<div class="col-span-3">
+	<div class="col-span-6 sm:col-span-3">
 		<div class="label">Bill To:</div>
 		<p>
 			<strong>{data.invoice.client.name}</strong><br />
@@ -96,7 +98,7 @@
 		</p>
 	</div>
 
-	<div class="col-span-2 col-start-5">
+	<div class="col-span-6 sm:col-span-2 sm:col-start-5">
 		<div class="label">Invoice ID</div>
 		<p>{data.invoice.invoiceNumber}</p>
 	</div>
@@ -106,7 +108,7 @@
 		<p>{convertDate(data.invoice.dueDate)}</p>
 	</div>
 
-	<div class="col-span-2 col-start-5">
+	<div class="col-span-3 col-start-5 sm:col-span-2">
 		<div class="label">Issue Date</div>
 		<p>{convertDate(data.invoice.issueDate)}</p>
 	</div>
