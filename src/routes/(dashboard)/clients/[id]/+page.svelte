c5ba9812e059a473abeb534972e173ebe8712c3f
Michael Lee <michaellee2245@gmail.com>
2023-05-25 00:38:31 -0600

    created helper functions to add dynamic money data for client balance

--- src/routes/(dashboard)/clients/[id]/+page.svelte
+++ src/routes/(dashboard)/clients/[id]/+page.svelte
@@ -7,13 +7,13 @@
 	import { centsToDollars, sumInvoices } from '$lib/utils/moneyHelpers';
 	import BlankState from '../../invoices/BlankState.svelte';
 	import InvoiceRowHeader from '../../invoices/InvoiceRowHeader.svelte';
-	import Portal from '$lib/components/Portal.svelte';
 	import Button from '$lib/components/Button.svelte';
 	import SlidePanel from '$lib/components/SlidePanel.svelte';
 	import Edit from '$lib/components/Icon/Edit.svelte';
 	import ClientForm from '../ClientForm.svelte';
+	import { isLate } from '$lib/utils/dateHelpers';
 
-	export let data;
+	export let data: { client: Client };
 	console.log({ data });
 
 	let isClientFormShowing: boolean = false;
@@ -32,6 +32,36 @@
 
 	const closePanel = () => {
 		isClientFormShowing = false;
+	};
+
+	const getDraft = (): string => {
+		if (!data.client.invoices || data.client.invoices.length < 1) return '0';
+		const draftInvoices = data.client.invoices.filter(
+			(invoice) => invoice.invoiceStatus === 'draft'
+		);
+		return centsToDollars(sumInvoices(draftInvoices));
+	};
+
+	const getPaid = (): string => {
+		if (!data.client.invoices || data.client.invoices.length < 1) return '0';
+		const paidInvoices = data.client.invoices.filter((invoice) => invoice.invoiceStatus === 'paid');
+		return centsToDollars(sumInvoices(paidInvoices));
+	};
+
+	const getTotalOverdue = (): string => {
+		if (!data.client.invoices || data.client.invoices.length < 1) return '0';
+		const paidInvoices = data.client.invoices.filter(
+			(invoice) => invoice.invoiceStatus === 'sent' && isLate(invoice.dueDate)
+		);
+		return centsToDollars(sumInvoices(paidInvoices));
+	};
+
+	const getTotalOutstanding = (): string => {
+		if (!data.client.invoices || data.client.invoices.length < 1) return '0';
+		const paidInvoices = data.client.invoices.filter(
+			(invoice) => invoice.invoiceStatus === 'sent' && !isLate(invoice.dueDate)
+		);
+		return centsToDollars(sumInvoices(paidInvoices));
 	};
 </script>
 
@@ -66,19 +96,19 @@
 <div class="mb-10 grid grid-cols-1 gap-4 rounded-lg bg-gallery py-7 px-10 lg:grid-cols-4">
 	<div class="summary-block">
 		<div class="label">Total Overdue</div>
-		<div class="number"><sup>$</sup>300.00</div>
+		<div class="number"><sup>$</sup>{getTotalOverdue()}</div>
 	</div>
 	<div class="summary-block">
 		<div class="label">Total Outstanding</div>
-		<div class="number"><sup>$</sup>300.00</div>
+		<div class="number"><sup>$</sup>{getTotalOutstanding()}</div>
 	</div>
 	<div class="summary-block">
 		<div class="label">Total Draft</div>
-		<div class="number"><sup>$</sup>300.00</div>
+		<div class="number"><sup>$</sup>{getDraft()}</div>
 	</div>
 	<div class="summary-block">
 		<div class="label">Total Paid</div>
-		<div class="number"><sup>$</sup>300.00</div>
+		<div class="number"><sup>$</sup>{getPaid()}</div>
 	</div>
 </div>
 
@@ -87,7 +117,7 @@
 	<!-- invoices -->
 	{#if data.client.invoices === null}
 		Loading...
-	{:else if data.client.invoices.length <= 0}
+	{:else if data.client?.invoices.length <= 0}
 		<BlankState />
 	{:else}
 		<InvoiceRowHeader className="text-daisyBush" />
