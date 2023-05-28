2a7c56712f85ceb1149cb480e39569353a132ea0
Michael Lee <michaellee2245@gmail.com>
2023-05-28 00:23:07 -0600

    added actions for touch screens

--- src/lib/actions/Swipe.ts
+++ src/lib/actions/Swipe.ts
@@ -8,7 +8,7 @@ interface SwipeProps {
 export const swipe: Action<HTMLElement, SwipeProps> = (node, params) => {
     let x: number;
     let startingX: number;
-    const elementWidth = node.clientWidth;
+    let elementWidth = node.clientWidth;
     let triggerReset = params?.triggerReset || false
 
     const coordinates = spring(
@@ -23,7 +23,32 @@ export const swipe: Action<HTMLElement, SwipeProps> = (node, params) => {
         node.style.transform = `translate3d(${$coords.x}px, 0, 0)`
     })
 
-    node.addEventListener('mousedown', handleMouseDown)
+    function setupEventListeners() {
+        if (isMobileBreakpoint()) {
+            node.addEventListener('mousedown', handleMouseDown)
+            node.addEventListener('touchstart', handleTouchStart)
+        }
+        else {
+            node.removeEventListener('mousedown', handleMouseDown)
+            node.removeEventListener('touchstart', handleTouchStart)
+        }
+        //update the card width
+        elementWidth = node.clientWidth
+    }
+
+    setupEventListeners()
+
+    //listen for our browser resize
+    window.addEventListener('resize', () => {
+        setupEventListeners()
+    })
+
+    function isMobileBreakpoint() {
+        const mediaQuery = window.matchMedia('(max-width: 1024px)');
+        if (mediaQuery.matches) {
+            return true
+        }
+    }
 
     function resetCard() {
         coordinates.update(() => {
@@ -40,28 +65,44 @@ export const swipe: Action<HTMLElement, SwipeProps> = (node, params) => {
     }
 
     function handleMouseDown(event: MouseEvent) {
-
         x = event.clientX;
         startingX = event.clientX;
         window.addEventListener('mousemove', handleMouseMove)
         window.addEventListener('mouseup', handleMouseUp)
     }
 
+    function handleTouchStart(event: TouchEvent) {
+
+        x = event.touches[0].clientX;
+        startingX = event.touches[0].clientX;
+        window.addEventListener('touchmove', handleTouchMove)
+        window.addEventListener('touchend', handleTouchEnd)
+    }
+
     function handleMouseMove(event: MouseEvent) {
         //Delta x = difference from where we clicked vs where we are currently
         const dx = event.clientX - x;
         x = event.clientX;
+        updateCoordinates(dx)
+    }
+
+    function handleTouchMove(event: TouchEvent) {
+        //Delta x = difference from where we clicked vs where we are currently
+        const dx = event.touches[0].clientX - x;
+        x = event.touches[0].clientX
+
+    }
+
+    function updateCoordinates(dx: number) {
         coordinates.update(($coords) => {
             return {
                 x: $coords.x + dx,
                 y: 0
             }
         })
-
-
     }
 
-    function updateCoordinates(x) {
+    function setXCoordinates(x: number) {
         coordinates.update(() => {
             return { x, y: 0 }
         })
@@ -81,8 +122,15 @@ export const swipe: Action<HTMLElement, SwipeProps> = (node, params) => {
         else {
             x = rightSnapX;
         }
-        updateCoordinates(x)
+        setXCoordinates(x)
     }
+    function handleTouchEnd(event: TouchEvent) {
+        const endingX = event.changedTouches[0].clientX
+        moveCardOver(endingX);
+        window.removeEventListener('touchmove', handleTouchMove)
+        window.removeEventListener('touchend', handleTouchEnd)
+    }
+
     function handleMouseUp(event: MouseEvent) {
         const endingX = event.clientX;
         moveCardOver(endingX);
@@ -98,6 +146,7 @@ export const swipe: Action<HTMLElement, SwipeProps> = (node, params) => {
         },
         destroy() {
             node.removeEventListener('mousedown', handleMouseDown)
+            node.removeEventListener('touchstart', handleTouchStart)
         }
     }
 }
