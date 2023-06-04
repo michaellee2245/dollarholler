529c460964e7f4b72517b12b6787a1d855e89346
Michael Lee <michaellee2245@gmail.com>
2023-06-03 23:51:29 -0600

    set up Authenticated and Unathenticated components to check for an active user session and redirect to the correct page based on if a user has a current session or not

---
+++ src/lib/components/Unauthenticated.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import supabase from '$lib/utils/supabase';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		const {
+			data: { session },
+			error
+		} = await supabase.auth.getSession();
+
+		if (session) {
+			await goto('/invoices');
+			return;
+		}
+	});
+</script>
+
+<slot><!-- optional fallback --></slot>
--- src/routes/(dashboard)/+layout.svelte
+++ src/routes/(dashboard)/+layout.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
+	import Authenticated from '$lib/components/Authenticated.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Snackbar from '$lib/components/Snackbar.svelte';
 </script>
 
-<Snackbar />
+<Authenticated>
+	<Snackbar />
 
-<div class="grid min-h-screen grid-cols-12 bg-whisper md:gap-x-16">
-	<Navbar />
+	<div class="grid min-h-screen grid-cols-12 bg-whisper md:gap-x-16">
+		<Navbar />
 
-	<main class="col-span-12 px-4 py-4 md:col-span-8 md:pt-10">
-		<slot><!-- Optional Fallback --></slot>
-	</main>
-</div>
+		<main class="col-span-12 px-4 py-4 md:col-span-8 md:pt-10">
+			<slot><!-- Optional Fallback --></slot>
+		</main>
+	</div>
+</Authenticated>
 
 <style lang="postcss">
 </style>
